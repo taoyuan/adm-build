577a04a2cc6c52cf06f960d279ab99cd2d6abbbb
taoyuan <torworx@gmail.com>
2016-03-06 19:24:20 +0800

    Update

--- bin/adm-build.js
+++ bin/adm-build.js
@@ -7,43 +7,19 @@ var yargs = require('yargs');
 var logger = require('adm-logger');
 var pkg = require('../package.json');
 
-var argv = process.argv;
+var argv = process.argv.slice(2);
 var $0 = process.env.CMD ? process.env.CMD : path.basename(argv[1]);
 var app = $0.split(' ')[0];
 logger.log = logger.createLog(app, {level: logger.log.level});
 
-yargs(argv.slice(2))
+var build = require('..');
+yargs(argv)
   .usage('Usage: ' + $0 + ' <target> [options]')
   .version(pkg.version)
   .alias('V', 'version')
   .help('h')
   .alias('h', 'help')
-  .options({
-    'a': {
-      alias: 'arch',
-      choices: ['all', 'armhf', 'amd64', 'none'],
-      default: 'armhf',
-      describe: 'Set target arch'
-    },
-    'f': {
-      alias: 'format',
-      choices: ['all', 'deb', 'snappy', 'none'],
-      default: 'deb',
-      describe: 'Specify the package format'
-    },
-    't': {
-      alias: 'type',
-      choices: ['auto', 'driver', 'app', 'core'],
-      default: 'auto',
-      describe: 'Specify the package type for dobox'
-    },
-    'r': {
-      alias: 'root',
-      type: 'string',
-      default: '/opt/dolink',
-      describe: 'Set the installation root path'
-    }
-  });
+  .options(build.options || {});
 
 var options = yargs.argv;
 
@@ -51,7 +27,7 @@ if (!options._.length) {
   return yargs.showHelp();
 }
 
-require('..')(options);
+build(options);
 
 
 
